268591baf20a1224873b8a077f75c70b12a2378c
Deven Phillips <InfoSec812@users.noreply.github.com>
2018-11-10 11:32:59 -0500

    Update uid_entrypoint.sh

--- configuration/uid_entrypoint.sh
+++ configuration/uid_entrypoint.sh
@@ -3,7 +3,7 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
 if [ x"$USER_ID" != x"0" ]; then
-
     echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> /etc/passwd
-
 fi
+
+$@
